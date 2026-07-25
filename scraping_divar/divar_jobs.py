59c7ac5b90323ba8eba24cd33f32cf34c6dc0a54
sobhan-al <alavi.s2001@gmail.com>
2026-07-25 14:22:01 +0330

    datas are complite, until clickable button for show more items.

--- scraping_divar/divar_jobs.py
+++ scraping_divar/divar_jobs.py
@@ -11,84 +11,63 @@ driver = webdriver.Chrome()
 # url = "https://divar.ir/s/isfahan/real-estate?map_interaction=search_this_area_disabled"
 url = "https://divar.ir/s/isfahan/jobs"
 driver.get(url)
-
 last_height = driver.execute_script("return document.body.scrollHeight")
 
 
-file_path = Path(__file__).parent / "home_datas.txt"
+file_path = Path(__file__).parent / "job_datas.txt"
 f = open(file_path,'w',encoding='utf8')
 
 
 
 def details(workss):
+    print(len(workss))
+    list_details = []
     for work in workss:
-        list_details = []
+
+        list_test = []
         title = work.find_element(By.CSS_SELECTOR, "h2.kt-post-card__title").text
-        list_details.append(title)
+        list_test.append(title)
         price = work.find_elements(By.CSS_SELECTOR, "div.kt-post-card__description")
         for i in price:
-            list_details.append(i.text)
+            list_test.append(i.text)
         location = work.find_element(By.CSS_SELECTOR, "div.kt-post-card__bottom span.kt-post-card__bottom-description").text
-        list_details.append(location)
+        list_test.append(location)
 
-        return list_details
+        list_details.append(list_test)
 
-
+    return list_details
 
 while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    time.sleep(7)
+    time.sleep(3)
     new_height = driver.execute_script("return document.body.scrollHeight")
     # works = driver.find_elements(By.CSS_SELECTOR, "div.kt-post-card__info")
-    works = driver.find_element(By.CSS_SELECTOR, "div.post-list-eb562")
-
+    works = driver.find_element(By.CSS_SELECTOR, "div.post-list-eb5625555555")
+    print(works.text)
+    time.sleep(5)
     works2 = works.find_elements(By.CSS_SELECTOR, "div.post-list__items-container-e44b2")
-
-    jobs = details(works2)
+    works3 = works.find_elements(By.CSS_SELECTOR, "div.widget-col-d2306")
+    jobs = details(works3)
     for job in jobs:
-        f.write(job+"\n")
-        time.sleep(5)
-    f.write("\n")    
+        for str in job:
+            f.write(str+"\n")
+        f.write("\n____________________\n")    
+
     if last_height == new_height:
+
         break
     else:
         last_height = new_height
 
-    f.write("mioooooooooooooooooooooo")    
+
+
+
+
+
+
 
 
 
 
 f.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
